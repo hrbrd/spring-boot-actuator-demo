5450995b3200f8ac685a76a5834931472b7a2f25
hrbrd <jacekharaburda1@gmail.com>
2024-04-19 16:22:44 +0200

    Metrics with Prometheus and Grafana

--- src/main/java/com/hrbrd/springbootmetricsdemo/HomeController.java
+++ src/main/java/com/hrbrd/springbootmetricsdemo/HomeController.java
@@ -1,13 +1,20 @@
 package com.hrbrd.springbootmetricsdemo;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final MeterRegistry meterRegistry;
+
+    public HomeController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
 
     @GetMapping("/hello")
     public String hello() {
+        meterRegistry.counter("hello").increment();
         return "Hello World!";
     }
 
